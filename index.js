f9c51f3b87f0f112bb8a577d167dc2ad1f8f9ea3
Richard van der Dys <richard.vanderdys@gmail.com>
2019-04-17 13:17:34 +0300

    Trying PubNub

--- index.js
+++ index.js
@@ -1,20 +1,31 @@
 require('dotenv').config();
-const createQueue = require('siju');
+// const createQueue = require('siju');
 const debug = require('debug');
+const PubNub = require('pubnub');
 const read = require('./read');
-const report = require('./report');
+// const report = require('./report');
 
 const log = debug('watson:reporter:moisture');
-const queue = createQueue(report);
+// const queue = createQueue(report);
+
+const pubnub = new PubNub({
+  publishKey: process.env.PUBLISH_KEY,
+  subscribeKey: process.env.SUBSCRIBE_KEY
+});
 
 function takeReading() {
   const reading = read.current;
   if (reading) {
-    queue.add(reading);
+    pubnub.publish({
+      "channel": "moistures",
+      "message": reading
+    });
+    // queue.add(reading);
   }
 }
 
-const TEN_MINS = 1000 * 60 * 10 + '';
+// const TEN_MINS = 1000 * 60 * 10 + '';
+const TEN_MINS = 1000 * 60;
 const interval = parseInt(process.env.INTERVAL || TEN_MINS, 10);
 console.log('Interval set at', interval, 'millis');
 
